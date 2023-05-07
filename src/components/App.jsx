be4944a3fc2e0ad6ce1006173a660ab8f40ae06c
annafrymark <anna.frymark@gmail.com>
2023-05-08 00:17:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import css from './app.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch])
+
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm />
+      {isLoading && !error && <h4>Loading, please wait...</h4>}
+      {error}
       <h2>Contacts</h2>
       <Filter />
       <ContactList />
